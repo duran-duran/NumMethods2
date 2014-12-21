9a17acf4f06da32c95810e2d375193dfb37a3bd4
duran-duran <bender1511@gmail.com>
2014-12-22 00:41:01 +0300

    Initial commit. PostfixNotaion class has been created

---
+++ NumMethods2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumMethods2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string input = Input.Text;
+            PostfixNotation notaion = new PostfixNotation(input);
+            try
+            {
+                double res = notaion.Calculate(3);
+                MessageBox.Show(res.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Запись функции имеет неверный формат");
+            }
+
+
+        }
+    }
+}
